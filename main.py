63fa7430573f54d11551d80a8f47b4883af5290e
akramfd12 <umarofahrul@gmail.com>
2026-02-21 15:03:27 +0700

    feat: add uvicorn for testing api

--- main.py
+++ main.py
@@ -1,45 +1,45 @@
-from agents.chatbot_agent import *
+# from agents.chatbot_agent import *
 
-conversation_history = []
+# conversation_history = []
 
-while True:
-    user_input = input("\nUser: ").strip()
+# while True:
+#     user_input = input("\nUser: ").strip()
 
-    if not user_input:
-        continue
+#     if not user_input:
+#         continue
 
-    if user_input.lower() in ["exit", "quit", "keluar"]:
-        print("Bye 👋")
-        break
+#     if user_input.lower() in ["exit", "quit", "keluar"]:
+#         print("Bye 👋")
+#         break
 
-    # Tambahkan input user ke history
-    conversation_history.append(
-        {"role": "user", "content": user_input}
-    )
+#     # Tambahkan input user ke history
+#     conversation_history.append(
+#         {"role": "user", "content": user_input}
+#     )
 
-    result = agent.invoke(
-        {
-            "messages": conversation_history
-        },
-        context={"user_role": "expert"}
-    )
+#     result = agent.invoke(
+#         {
+#             "messages": conversation_history
+#         },
+#         context={"user_role": "expert"}
+#     )
 
-    # Ambil jawaban terakhir agent
-    agent_reply = result["messages"][-1].content
+#     # Ambil jawaban terakhir agent
+#     agent_reply = result["messages"][-1].content
 
-    print("\nAgent:", agent_reply)
+#     print("\nAgent:", agent_reply)
 
-    # Simpan jawaban agent ke history
-    conversation_history.append(
-        {"role": "assistant", "content": agent_reply}
-    )
+#     # Simpan jawaban agent ke history
+#     conversation_history.append(
+#         {"role": "assistant", "content": agent_reply}
+#     )
 
 
-# import os
-# from api.routes import app
+import os
+from api.routes import app
 
-# if __name__ == "__main__":
-#     import uvicorn
-#     port = int(os.environ.get("PORT", 8080))
-#     uvicorn.run("api.routes:app", host="0.0.0.0", port=port, factory=False)
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run("api.routes:app", host="0.0.0.0", port=port, factory=False)
 
